9f0601494a298396bbf58b85358780c7f6dd83f1
David Ireland <davidji@pobox.com>
2025-09-09 18:41:54 +0100

    Corrections to test

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import client, asyncio
 import client.ieee_802_15_4_pb2 as radio
 
 async def connect():
-    radio1 = await client.connect('192.168.3.1')
-    radio2 = await client.connect('192.168.4.1')
+    radio1 = await client.connect('192.168.3.16')
+    radio2 = await client.connect('192.168.4.8')
 
-    radio1.configure(channel=radio.C15, tx_power=radio.P0dBm)
-    radio2.configure(channel=radio.C15, tx_power=radio.P0dBm)
+    radio1.configure(channel=radio.C15, tx_power=radio.NEG4D_BM)
+    radio2.configure(channel=radio.C15, tx_power=radio.NEG4D_BM)
     async def send():
         for i in range(10):
             radio1.transmit(b'Hello from radio 1')
@@ -16,3 +16,4 @@ async def connect():
 
     await asyncio.gather(radio2.print_received(), send())
 
+asyncio.run(connect())
