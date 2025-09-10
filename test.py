e15a0fb2a99e256a18dd8710aa94928f2af21ce7
David Ireland <davidji@pobox.com>
2025-09-10 10:03:19 +0100

    debugging

--- test.py
+++ test.py
@@ -9,10 +9,13 @@ async def connect(sender_address, receiver_address):
     
     async def send():
         for channel in radio.Channel.values():
+            print("Setting channel", channel)
             radio1.configure(channel=channel, tx_power=radio.POS2D_BM)
             radio2.configure(channel=channel, tx_power=radio.POS2D_BM)
             for i in range(20):
+                print("Sending packet", i)
                 radio1.transmit(b'Hello from radio 1')
+                print("Sent packet", i)
                 await asyncio.sleep(0.1)
 
     await asyncio.gather(radio1.print_received(), radio2.print_received(), send())
